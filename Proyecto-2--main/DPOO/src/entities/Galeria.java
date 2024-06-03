0fdfdd16d2baa817f5156be89860c563501649b8
ndelacruz2 <141865447+ndelacruz2@users.noreply.github.com>
2024-06-02 21:15:43 -0500

    Update Galeria.java

--- Proyecto-2--main/DPOO/src/entities/Galeria.java
+++ Proyecto-2--main/DPOO/src/entities/Galeria.java
@@ -2,6 +2,7 @@ package entities;
 
 import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.List;
 
 import logic.Cashier;
 import logic.Comprador;
@@ -11,6 +12,7 @@ import logic.admin;
 
 public class Galeria {
 	
+	
 	private static Inventario inventario;
 	private static ArrayList<Subasta> Subastas = new ArrayList<Subasta>();
 	private static admin administrador;
@@ -18,20 +20,90 @@ public class Galeria {
 	private static Hashtable<String, Operador> operadores = new Hashtable<>();
 	private static Hashtable<String, Comprador> compradores = new Hashtable<>();
 	private static Hashtable<String, Comprador> propietarios = new Hashtable<>();
-	
-	
-	
-	public static ArrayList<Subasta> subastas(){
-		return Subastas;
-	    }
-	
-	public static void setcajero(String usuario, Cashier cajero) {
-		
-	   cajeros.put(usuario, cajero);
-			
+	public static List<Pieza> piezas = new ArrayList<Pieza>();
+
+	public static List<Pieza> getPiezas() {
+		return piezas;
+	}
+
+	public static List<String> getArtistas() {
+		List<String> artistas = new ArrayList<>();
+		for (Pieza pieza : piezas) {
+			String autor = pieza.getAutores();
+			if (!artistas.contains(autor)) {
+				artistas.add(autor);
+			}
 		}
-		
-    public static void setoperador(String usuario,Operador operador) {
+		return artistas;
+	    }
+
+	public static void addPieza(Pieza pieza) {
+		piezas.add(pieza);
+	}
+
+	public static Hashtable<String,Comprador> getCompradores() {
+		return compradores;
+	}
+
+	public static Pieza buscarPieza(String nombre) {
+		for (Pieza pieza : piezas) {
+			if (pieza.getTitulo().equals(nombre)) {
+				return pieza;
+			}
+		}
+		return null;
+	}
+
+	public static String buscarArtista(String nombre) {
+		for (Pieza pieza : piezas) {
+			if (pieza.getAutores().equals(nombre)) {
+				return pieza.getAutores();
+			}
+		}
+		return null;
+	}
+
+	public static Comprador buscarComprador(String nombre) {
+				for (Comprador comprador : compradores.values()) {
+					if (comprador.getnombre().equals(nombre)) {
+						return comprador;
+					}
+				}
+				return null;
+			}
+	
+	public static void crearSubastas(Pieza pieza, double precio_inicial)
+	{
+		Subasta subasta = new Subasta(pieza, precio_inicial);
+		Subastas.add(subasta);
+	
+	}
+
+	public static Subasta accederSubasta(int codigo)
+	{
+		for (Subasta subasta : Subastas) {
+			if (subasta.getCodigo() == codigo) {
+				return subasta;
+			}
+		}
+		return null;
+	}
+
+	public static boolean verificarOferta(int idSubasta, double oferta)
+	{
+		Subasta subasta = accederSubasta(idSubasta);
+		if (subasta != null) {
+			return subasta.verificarOferta(oferta);
+		}
+		return false;
+	}
+	public static void setcajero(String usuario, Cashier cajero) {
+				
+			   cajeros.put(usuario, cajero);
+					
+				}
+				
+	public static void setoperador(String usuario,Operador operador) {
 			
 			operadores.put(usuario, operador);
 		}
@@ -76,8 +148,10 @@ public class Galeria {
     	return propietarios.get(usuario);
     	
     }
-    
-    
-		
-	}
 
+	public void removePieza(Pieza pieza) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'removePieza'");
+	}
+	
+	}
